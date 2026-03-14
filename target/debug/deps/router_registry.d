1dc8b454ff393b6e326dfe900e88fb6aa1abaec8
austinaminu2 <austinaminu2@gmail.com>
2026-03-14 22:58:36 +0100

    chore: add .gitignore, remove target from tracking

--- target/debug/deps/router_registry.d
+++
@@ -1,7 +0,0 @@
-/home/ncdndjdj/stellar-router/target/debug/deps/router_registry.d: contracts/router-registry/src/lib.rs
-
-/home/ncdndjdj/stellar-router/target/debug/deps/librouter_registry.so: contracts/router-registry/src/lib.rs
-
-/home/ncdndjdj/stellar-router/target/debug/deps/librouter_registry.rlib: contracts/router-registry/src/lib.rs
-
-contracts/router-registry/src/lib.rs:
